60ee568dfdd1551d38a80ee200b57e95ed613377
Kgodisho Mononyane <kgodisho.mononyane@gmail.com>
2025-11-15 09:03:05 +0200

    add dialog box to startBtn event listener

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
+//QUERY SELECTORS AND DECLARATIONS
 const startBtn = document.querySelector("#start-btn")
+const dialogBox = document.querySelector("#dialog-box");
 
 
+
+//EVENT LISTENERS
 startBtn.addEventListener("click", () => {
     console.log("startBtn clicked")
+    dialogBox.showModal();
 })
+
+
+
+//FUNCTIONS
