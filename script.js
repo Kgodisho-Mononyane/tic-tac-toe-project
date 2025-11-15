293de94cebfa04682e9abe840a414567d04a8788
Kgodisho Mononyane <kgodisho.mononyane@gmail.com>
2025-11-15 12:53:04 +0200

    add form inputs, basic form styling and submit button event listener

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //QUERY SELECTORS AND DECLARATIONS
 const startBtn = document.querySelector("#start-btn")
 const dialog = document.querySelector("#dialog");
+const submitBtn = document.querySelector("#submitBtn")
 
 
 
@@ -8,6 +9,10 @@ const dialog = document.querySelector("#dialog");
 startBtn.addEventListener("click", () => {
     console.log("startBtn clicked")
     dialog.showModal();
+})
+
+submitBtn.addEventListener("click", (e) => {
+    e.preventDefault();
 })
 
 
