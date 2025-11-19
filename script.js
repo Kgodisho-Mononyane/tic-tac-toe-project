e37ddb0e56ba04f93428d28d36e13b741dcd679f
Kgodisho Mononyane <kgodisho.mononyane@gmail.com>
2025-11-19 09:38:33 +0200

    add to startBtn and submitBtn eventlisteners

--- script.js
+++ script.js
@@ -2,14 +2,24 @@
     const startBtn = document.querySelector("#start-btn");
     const dialog = document.querySelector("#dialog");
     const submitBtn = document.querySelector("#submitBtn");
+    const form = document.querySelector("#form");
+    const playerX = document.querySelector("#playerX").value;
+    const playerO = document.querySelector("#playerO").value;
     
     
     startBtn.addEventListener("click", () => {
-        console.log("startBtn clicked")
         dialog.showModal();
+        form.reset();
     })
 
     submitBtn.addEventListener("click", (e) => {
         e.preventDefault();
+        dialog.close();
     })
+
+    return {playerX, playerO};
+})();
+
+const test = (function() {
+    console.log(playerX.value, playerO.value);
 })();
