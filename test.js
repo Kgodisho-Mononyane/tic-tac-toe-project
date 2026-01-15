b09c7f7213a940631a6455b22380a48bb6b8ea7c
Kgodisho Mononyane <kgodisho.mononyane@gmail.com>
2026-01-15 15:01:34 +0200

    add pseudocode

---
+++ test.js
@@ -0,0 +1,28 @@
+/*
+FUNCTION gameBoard
+    SET rows to 3
+    SET columns to 3
+    SET board to []
+
+    FOR let i = 0; i < rows; i++
+        board[i] = [];
+        FOR let j = 0; j < columns; j++
+
+
+    ENDFOR
+
+    SET getBoard to board
+
+    RETURN
+        getBoard,
+        
+ENDFUNCTION    
+
+FUNCTION playGame
+ENDFUNCTION
+
+FUNCTION screenController
+ENDFUNCTION
+
+
+*/
