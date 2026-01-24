7a051e49b594db69f5dc5b9bc2d1f1688f48b744
Kgodisho Mononyane <kgodisho.mononyane@gmail.com>
2026-01-24 15:41:38 +0200

    add screenController function

--- test.js
+++ test.js
@@ -26,9 +26,9 @@ FUNCTION gameBoard
         clearBoard,
         placeToken
 ENDFUNCTION    
+*/
 
-
-
+/*
 FUNCTION playGame
     SET board to getBoard
     SET activePlayer to player X
@@ -70,6 +70,15 @@ FUNCTION playGame
             play again
     ENDFUNCTION
     
+    FUNCTION checkGameWinner
+        IF PlayerX or PlayerO score = 3 THEN
+            CALL declarewinner
+    ENDFUNCTION
+    
+    FUNCTION declareWinner
+
+    ENDFUNCTION
+    
     FUNCTION playAgain
         clear board
         set activePlayer to player X
@@ -84,38 +93,48 @@ FUNCTION playGame
     RETURN
     ENDRETURN
 ENDFUNCTION
+*/
 
 
+function screenController() {
+    //SET game to playGame
+    //SET board to getBoard
+    //SET playerXName
+    //SET playerOName
+    const startBtn = document.querySelector("#start-btn");
+    const submitBtn = document.querySelector("#submitBtn");
+    const playAgainBtn = document.querySelector("#play-again-btn");
+    const resetBtn = document.querySelector("#resetBtn");
 
-FUNCTION screenController
-    SET game to playGame
-    SET board to getBoard
-    SET startBtn
-    SET submitBtn
-    SET playAgainBtn
-    SET resetBtn
-
+    /*
     EVENT board
         make board interactable on the dom
     ENDEVENT
-    
-    EVENT startBtn
-        open dialog box
-    ENDEVENT
-    
-    EVENT submitBtn
-    ENDEVENT
-    
-    EVENT playAgainBtn
-        CALL playAgain
-    ENDEVENT
-    
-    EVENT resetBtn
-        CALL reset
-    ENDEVENT
+    */
 
-ENDFUNCTION
+    startBtn.addEventListener("click", () => {
+        //open dialogbox
+    })
+     
+    submitBtn.addEventListener("click", () => {
+        /*
+        IF user entered names in the input fields THEN
+            playerXName = name given in input field
+            playerOName = name given in input field
+        ELSE
+            default to the original names
+        ENDIF
+        */
+    })
+    
+    playAgainBtn.addEventListener("click", () => {
+        //CALL playAgain
+    })
+    
+    resetBtn.addEventListener("click", () => {
+        //CALL reset
+        //playerXName = Player X
+        //playerOName = player O  
+    })
 
-
-
-*/
+}
